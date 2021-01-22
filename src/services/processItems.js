ea8ce84794da3f9b926ed3c628aad402dc783ec4
Ana Lima <analimazn@gmail.com>
2021-01-22 00:34:57 -0300

    feat: Added function process items list

--- src/services/processItems.js
+++ src/services/processItems.js
@@ -20,7 +20,7 @@ function createItemsList(quantityItems, quantityEmails) {
 
   if (quantityEmails) {
     while (quantityEmails > 0) {
-      const email = `email@${Math.floor(Math.random() * 100)}.com`
+      const email = `email@${Math.floor(Math.random() * 100)}.com`;
 
       emails.push(email);
       quantityEmails--;
@@ -31,17 +31,40 @@ function createItemsList(quantityItems, quantityEmails) {
   return data;
 }
 
-function calculateItemsList() {
+function calculateItemsList(items) {
+  const total = items.reduce((acc, item) => {
+    acc+= item.quantity * item.price;
+    return acc;
+  }, 0);
 
+  return total
+}
+
+function divideItemsList(total, emails) {
+  const division = Math.floor(total / emails.length);
+  const rest = total - (division * emails.length);
+  const emailsToDivide = emails[rest];
+  console.log(emailsToDivide)
   return
 }
 
-function divideItemsList() {
+function processItemsList(data) {
+  if (!data.items.length || !data.emails.length) {
+    return "Sua lista de emails e/ou itens está vazia";
+  }
+
+  const total = calculateItemsList(data.items);
+
+  if (total === 0) {
+    return "Sua lista de itens não pode ser dividida";
+  }
+
+  const result = divideItemsList(total, data.emails)
+
   return
 }
 
 module.exports = {
   createItemsList,
-  calculateItemsList,
-  divideItemsList
+  processItemsList
 }
